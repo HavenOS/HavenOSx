289b02c55815bcff4d5e71eb072fdc76a4ad53ac
Big Evil <lusji2011@gmail.com>
2024-01-13 16:49:25 -0300

    device

---
+++ device/xiaomi/ginkgo/AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_ginkgo-user \
+    aosp_ginkgo-userdebug \
+    aosp_ginkgo-eng
